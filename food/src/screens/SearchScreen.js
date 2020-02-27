c63b9a4511212467ee02b9032dfdfc9878d3a596
ventsi <v.saraminev@abv.bg>
2020-02-27 23:35:36 +0200

    finished app

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,22 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
    const [term, setTerm] = useState('');
    const [searchApi, results, errorMessage] = useResults();
+   const filterResultsByPrice = (price) => {
+      return results.filter(result => {
+         return result.price === price
+      });
+   };
 
    return (
-      <View>
+      <>
          <SearchBar 
             term={term}
             onTermChange={newTerm => setTerm(newTerm)}
             onTermSubmit={() => searchApi(term)}
          />
          {errorMessage ? <Text>{errorMessage}</Text> : null}
-         <Text>We have found {results.length} results</Text>
-      </View>
+         <ScrollView>
+            <ResultsList 
+               title="Cost Effective" 
+               results={filterResultsByPrice('$')}
+            />
+            <ResultsList 
+               title="Bit Pricier" 
+               results={filterResultsByPrice('$$')}
+            />
+            <ResultsList 
+               title="Big Spender" 
+               results={filterResultsByPrice('$$$')}
+            />
+         </ScrollView>
+      </>
    );
 };
 
