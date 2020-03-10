8cef69a8e57fe34ca1ab0cc8ef5a05d2a8f5d46e
ventsi <v.saraminev@abv.bg>
2020-03-10 22:46:16 +0200

    Added showScreen and CreateScreen files

--- blog/App.js
+++ blog/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './src/screens/IndexScreen';
-import { BlogProvider } from './src/context/BlogContext';
+import { Provider } from './src/context/BlogContext';
+import ShowScreen from './src/screens/ShowScreen';
+import CreateScreen from './src/screens/CreateScreen';
 
 const navigator = createStackNavigator({
-  Index: IndexScreen
+  Index: IndexScreen,
+  Show: ShowScreen,
+  Create: CreateScreen
 }, {
   initialRouteName: 'Index',
   defaultNavigationOptions: {
@@ -16,7 +20,7 @@ const navigator = createStackNavigator({
 const App = createAppContainer(navigator);
 
 export default () => {
-  return <BlogProvider>
+  return <Provider>
       <App />
-  </BlogProvider>;
+  </Provider>;
 };
--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -1,25 +1,57 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
-import BlogContext from '../context/BlogContext';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
+import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
-const IndexScreen = () => {
-   const { data, addBlogPost } = useContext(BlogContext);
+const IndexScreen = ({ navigation }) => {
+   const { state, addBlogPost, deleteBlogPost } = useContext(Context);
 
    return (
       <View>
-         <Text>Index Screen</Text>
          <Button title = "Add Post" onPress = {addBlogPost} />
          <FlatList 
-         data = {data}
+         data = {state}
          keyExtractor = {(blogPosts) => blogPosts.title}
          renderItem = {({ item }) => {
-            return <Text>{item.title}</Text>
+            return ( 
+            <TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}>
+               <View style={styles.row}>
+                  <Text  style={styles.title}>{item.title} - {item.id}</Text>
+                  <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                     <Feather style={styles.icon} name="trash" />
+                  </TouchableOpacity>
+               </View>
+            </TouchableOpacity>
+            );
          }}
          />
       </View>
    );
 };
 
-const styles = StyleSheet.create({});
+IndexScreen.navigationOptions = ({ navigation }) => {
+   return {
+      headerRight: () => <TouchableOpacity 
+         onPress={() => navigation.navigate('Create')}>
+            <Feather name="plus" size={30} />
+         </TouchableOpacity>
+   };
+};
+const styles = StyleSheet.create({
+   row: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      paddingVertical: 20,
+      paddingHorizontal: 10,
+      borderTopWidth: 1,
+      borderColor: 'grey'
+   },
+   title: {
+      fontSize: 18
+   },
+   icon: {
+      fontSize: 24
+   }
+});
 
 export default IndexScreen;
